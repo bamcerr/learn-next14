10965f6068068034cbca22b2c56c25f47fce9a6a
bamcerr <bamcerr@gmail.com>
2024-05-14 14:13:21 +0900

    Parallel Requests

--- app/(movie)/movies/[id]/page.tsx
+++ app/(movie)/movies/[id]/page.tsx
@@ -1,7 +1,33 @@
-export default function MovieDetail({
+import { API_URL } from "../../../(home)/page";
+
+async function getMovie(id:string) {
+  console.log(`fetching movie: ${Date.now()}`)
+  const response = await fetch(`${API_URL}/${id}`)
+  const json = await response.json()
+  return json
+}
+
+async function getVideos(id:string) {
+  console.log(`fetching videos: ${Date.now()}`)
+  const response = await fetch(`${API_URL}/${id}/videos`)
+  const json = await response.json()
+  return json
+}
+
+export default async function MovieDetail({
   params: {id}
 } : {
   params : { id: string }
 }) {
-  return <h1>Movie {id}</h1>;
+  console.log("===========");
+  console.log("start fetching")
+  const [movie, videos] = await Promise.all([
+    getMovie(id),
+    getVideos(id)
+  ])
+  console.log("end fetching");
+
+  return <h1>{movie.title}</h1>;
 }
+
+
