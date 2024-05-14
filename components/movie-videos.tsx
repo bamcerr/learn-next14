4e99f56582a3c20250fdedb46ae3e3a8cd5b1665
bamcerr <bamcerr@gmail.com>
2024-05-14 14:46:11 +0900

    Error

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,9 +1,11 @@
 import { API_URL } from "../app/(home)/page"
 
 async function getVideos(id:string) {
-  const response = await fetch(`${API_URL}/${id}/videos`)
-  const json = await response.json()
-  return json
+  // const response = await fetch(`${API_URL}/${id}/videos`)
+  // const json = await response.json()
+  // return json
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  throw new Error("something broke...")
 }
 
 
